5a042727d2dab3f8dffcfd21d70a6eab1264fc8a
tilafu <tilafumusic@gmail.com>
2022-07-07 01:23:01 +0000

    More alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - main
+ *
+ * function prints alphabet  10 times
+ *
+ * Return: null
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n')
+		i++;
+	}
+}
