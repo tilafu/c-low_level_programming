cba7dadaa098734deceaa88b75d45ada8c297d37
tilafu <tilafumusic@gmail.com>
2022-07-07 00:04:45 +0000

    Print c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+/*
+ * main -The main function
+ * Return: 0 success
+ */
+
+#include "main.h"
+/**
+ * main - main
+ * print putchar followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
+}
