4898cfbe7d9537ab66ee1d24a07368485071e6ba
tilafu <tilafumusic@gmail.com>
2022-07-07 01:51:49 +0000

    Print symbols

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints a sign with a number
+ *
+ * @n: the int numero
+ *
+ * Description: print +, 0, or - depending on number
+ *
+ * Return: 1 for positive, 0 for 0, and -1 for -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('_');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
+
