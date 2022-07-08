07f1380d195d9b77cfb5cae1e9e1cc90e5c483e5
tilafu <tilafumusic@gmail.com>
2022-07-08 23:56:21 +0000

    Some trial shit

---
+++ 0x04-more_functions_nested_loops/5-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * void print_line - This function prints a line
+ * @n: value to be used somewhere
+ * Retun : Always 0
+ */
+
+void print_line(int n)
+{
+	n = /*some input sth*/
+	while (n >= 0)
+	{
+		_putchar('__');
