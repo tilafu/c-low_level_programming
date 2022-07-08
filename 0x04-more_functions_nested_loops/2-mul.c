ece5ae8a6d42a3eb6e80a4f4cb508a3eeda34837
tilafu <tilafumusic@gmail.com>
2022-07-08 18:51:54 +0000

    How to multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - This functin multiplies 2 values
+ * @a: one of the values
+ * @b: The second of the values
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
