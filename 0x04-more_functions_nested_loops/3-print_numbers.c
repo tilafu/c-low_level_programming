464662cfd5573e7bd8a5ac8339051de8628117af
tilafu <tilafumusic@gmail.com>
2022-07-08 18:57:14 +0000

    How to print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - writes numbers from 0 to 9
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i = '9'; i++)
+	{
+		_putchar(c);
+	}
+	_putchar(c);
+}
