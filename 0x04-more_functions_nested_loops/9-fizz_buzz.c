ed808d8a9c1c0d5a53d6f4a403fd680b621c9091
tilafu <tilafumusic@gmail.com>
2022-07-09 02:30:58 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,25 @@ int main(void)
 	for (i > 0; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-		{
+
 			printf("Fizz");
-		}
+
 		else if (i % 5 == 0)
-		{
+
 			printf("Buzz")
-		}
+
 		else if (i % (5 * 3) == 0)
-		{
+
 			printf("FizzBuzz")
-		}
+
 		else
-		{
+
 			printf("%i", i)
-		}
+
 		if (i < 100)
-		{
+
 			printf(" ");
-		}
+
 		printf("\n");
 		return (0);
 	}
