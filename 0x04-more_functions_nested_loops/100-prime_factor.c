285716da064ee99cfd58812aa6074cfcaee26ac7
tilafu <tilafumusic@gmail.com>
2022-07-09 02:07:36 +0000

    Some primes

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest prime of a large number
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, n = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+	printf("%c\n", n);
+	return (0);
+}
