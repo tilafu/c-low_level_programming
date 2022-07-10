4cec8e8e8275a0a53454238b80f28c4fa73551d3
tilafu <tilafumusic@gmail.com>
2022-07-10 12:53:58 +0000

    Hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,31 +8,25 @@
 
 int main(void)
 {
-	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (int i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5))
 
-			printf("Fizz");
+			printf("Fizz Buzz\n");
+
+		else if (i % 3 == 0)
+
+			printf("Fizz\n");
 
 		else if (i % 5 == 0)
 
-			printf("Buzz");
-
-		else if (i % 15 == 0)
-
-			printf("FizzBuzz");
+			printf("Buzz\n");
 
 		else
 
-			printf("%i", i);
+			printf("%d", i);
 
-		if (i < 100)
-
-			printf(" ");
-
-		printf("\n");
 		return (0);
 	}
 }
