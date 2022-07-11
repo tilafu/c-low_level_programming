a7f582ed19ecc218ec1aec0d6f036c7934d129bc
tilafu <tilafumusic@gmail.com>
2022-07-11 22:21:42 +0000

    How to swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps 2 values using pointers
+ * @a: the first value
+ * @b: the second value
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*a = c;
+	*b = *a;
+	c = *b;
+}
