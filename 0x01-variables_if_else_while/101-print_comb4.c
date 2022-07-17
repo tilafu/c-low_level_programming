738ff9fcf86dc0be39bc65fc2ab78a2881672b04
tilafu <tilafumusic@gmail.com>
2022-07-17 12:29:57 +0000

    Some long a** s**t

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - program prints decimals
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; j++)
+	{
+		for (j = 49; j <= 56; j++)
+		{
+			for (k = 50; k <= 57; k++)
+			{
+				if ((i >= j) || (j >= k))
+					continue;
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if ((k == 57) && (j == 560)
+						{
+						if (i == 55)
+						break;
+						}
+						putchar(44);
+						putchar(32);
+						}
+						}
+						}
+						}
