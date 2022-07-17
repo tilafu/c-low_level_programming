1f2bd950f6bff2372120026eb70a6d34f92127d4
tilafu <tilafumusic@gmail.com>
2022-07-17 11:28:58 +0000

    Prints base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all decimal numbers
+ * Return: Exit with 0 if unsuccessful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
