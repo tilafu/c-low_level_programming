31f5e44d4d64f91e8daf931b3767d706a3e8126d
tilafu <tilafumusic@gmail.com>
2022-07-17 12:23:39 +0000

    Print decimals with putchar

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints decimal digits without printf
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if ((j == 57) && (i == 56))
+				break;
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
