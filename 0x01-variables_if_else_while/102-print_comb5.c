395423fb56b55920069c083c704cae57597c0560
tilafu <tilafumusic@gmail.com>
2022-07-17 15:33:08 +0000

    Combinations of numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,28 +2,38 @@
 
 /**
  * main - prints possible combinations
+ * Description: Print possible combinations of double digits
+ * All numbers are double digits e.g. 1 == '01'
+ * Can putchar only 8 times
  * Return: 0 if success
  */
+
 int main(void)
 {
 	int i, j;
+	int a, b, c, d;
 
-	for (i = 0; j < 100)
+	for (i = 0; i < 100; i++)
 	{
-		if (i < j)
+		a = i / 10;
+		b = i % 10;
+
+		if (a < c || (a == c && b < d))
 		{
-			putchar((i / 10) + 48);
-			putchar((i % 10) + 48);
+			putchar(a + '0');
+			putchar(b + '0');
 			putchar(32);
-			putchar((j / 10) + 48);
-			putchar((j % 10) + 48);
-			if (i != 98 || j != 99)
+			putchar(c + '0');
+			putchar(d + '0');
+
+			if (!(a == 9 && b == 8))
 			{
 				putchar(44);
 				putchar(32);
 			}
 		}
 	}
-	putchar('\n');
+	putchar(10);
+
 	return (0);
 }
