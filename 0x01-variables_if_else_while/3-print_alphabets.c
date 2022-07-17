3af75188d88607f2952f007b9875af0819864d61
tilafu <tilafumusic@gmail.com>
2022-07-17 11:20:55 +0000

    Prints alphabet in upper and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower case and upper case
+ * Return: Void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
