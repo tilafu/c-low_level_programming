884b54951613a60b15e4a2d5da7442115246e24a
tilafu <tilafumusic@gmail.com>
2022-07-19 22:42:24 +0000

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sums 2 numbers
+ * @num1: this is an integer
+ * @num2: this is int 2
+ * Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
