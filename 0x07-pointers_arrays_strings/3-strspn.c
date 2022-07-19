722360bc52c755839374b50c3d8708094173dcb2
tilafu <tilafumusic@gmail.com>
2022-07-19 21:04:49 +0000

    Hot fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,28 +9,19 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
-	int j;
-	int c;
+	unsigned int count = 0, supposed_count = 0;
+	int i, j;
 
-	i = 0;
-	c = 0;
-
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j]
-			{
-				c++;
-				break;
-			}
-
-			j++;
+			if (s[i] == accept[j])
+				count++;
 		}
-		if (accept[j] == '\0')
+		supposed_count++;
+		if (count == 0 || count != supposed_count)
 			break;
-		i++;
 	}
-	return (c);
+	return (count);
 }
