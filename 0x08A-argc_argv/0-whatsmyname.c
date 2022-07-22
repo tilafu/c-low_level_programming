2b60b23bbbaf878e7d59b57d274846d278ec72e7
tilafu <tilafumusic@gmail.com>
2022-07-22 23:59:43 +0000

    Whats my name?

---
+++ 0x08A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print filename followed by new line
+ * @argc: - count parameter
+ * @argv: - arguments passed to main as string array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
