701fd162934c76c6e7aa56286c6fa14f54360612
tilafu <tilafumusic@gmail.com>
2022-07-23 00:04:05 +0000

    Hot fix

--- 0x08A-argc_argv/0-whatsmyname.c
+++ 0x08A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - print filename followed by new line
  * @argc: - count parameter
@@ -8,8 +9,6 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
-
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
