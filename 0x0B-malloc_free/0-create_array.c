e26cfc59683609ff0c96ea285001db9e8813fa1d
tilafu <tilafumusic@gmail.com>
2022-07-27 01:41:34 +0000

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,6 +4,24 @@
  * create_array - fxn that creates arrays starting from a char c
  * @size: size of the array
  * @c: the first character of the array
+ * Return: pointer to the array or null
  */
 
+char *create_array(unsigned int size, char c)
+{
+	char *A;
+	unsigned int i = 0;
 
+	if (size == 0)
+		return (NULL);
+
+	A = malloc(sizeof(char) * size);
+	if (A == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		A[i] = c;
+		i++;
+	}
+	return (A);
+}
