0b06afea3be0813e422b677803aa4d4eddd4d84f
tilafu <tilafumusic@gmail.com>
2022-07-30 04:42:40 +0000

    Inconcludo

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenates 2 strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @n: limit of s2
+ *
+ * Return: poiter to new memory space
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *comb;
+	int i;
+	unsigned int j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		S2 = "";
+
+	i = 0;
+	while (s1[i])
+		i++;
+
+	i++;
+
+	comb = malloc(sizeof(*comb) * (i + n));
+	if (comb == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		comb[i] = s1[i];
+		i++;
+	}
+	for (j = 0; j < n
