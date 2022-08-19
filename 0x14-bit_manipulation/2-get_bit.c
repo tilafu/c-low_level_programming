72942b347e0139bdb4065fad5f9d842048f6998b
tilafu <tilafumusic@gmail.com>
2022-08-19 15:45:28 +0000

    Binary to decimal

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * get_bit - returs the value of a bit
+ * @n: unsigned long int
+ * @index: index of the bit
+ * Return: value of the bit
+ */
+
+
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,5 +4,6 @@
 #include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif /*main_h*/
