46338d9d6c21aca5f38362ce8d678605520d77cd
tilafu <tilafumusic@gmail.com>
2022-08-20 18:45:25 +0000

    change counter

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,38 +12,37 @@ int main(int argc, char *argv[])
 {
 	int cents, ncoins = 0;
 
-	 if (argc == 1 || argc > 2)
-	 {
-		 printf("Error\n");
-		 return (1);
-	 }
+	if (argc < 1)
+	{
+		printf("%d\n", 0);
+	}
+	if (argc != 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-	 cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	 while (cents > 0)
-	 {
-		 if (cents >= 25)
-		 {
-			 cents -= 25;
-		 }
-		 if (cents >= 10)
-		 {
-			 cents -= 10;
-		 }
-		 if (cents >= 5)
-		 {
-			 cents -= 5;
-		 }
-		 else if (cents >= 2)
-		 {
-			 cents -= 2;
-		 }
-		 else if (cents >= 1)
-		 {
-			 cents -= 1;
-		 }
-		 ncoins += 1;
-	 }
-	 printf("%d\n". ncoins);
-	 return (0);
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+
+		if (cents >= 10)
+			cents -= 10;
+
+		if (cents >= 5)
+			cents -= 5;
+
+		else if (cents >= 2)
+			cents -= 2;
+
+		else if (cents >= 1)
+			cents -= 1;
+
+		ncoins += 1;
+	}
+	printf("%d\n". ncoins);
+	return (0);
 }
