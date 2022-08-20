be056c66b882e2e8ceb6b6e39aa2364f18f7fcf0
tilafu <tilafumusic@gmail.com>
2022-08-20 18:25:25 +0000

    change counter

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,59 +2,48 @@
 #include <stdlib.h>
 
 /**
- * isInteger - checks if s is integer
- * @s: string to check
- * Return: 0 or 1
+ * main - prints number of coins for change in an amount
+ * @argc: number of cmd arguments
+ * @argv: array containing command line arguments
+ * Return: 0 if succesful
  */
 
-int isInteger(const char *s)
+int main(int argc, char *argv[])
 {
-	int i = 0;
+	int cents, ncoins = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
+	 if (argc == 1 || argc > 2)
+	 {
+		 printf("Error\n");
+		 return (1);
+	 }
 
-		i++;
-	}
-	return (1);
-}
+	 cents = atoi(argv[1]);
 
-/**
- * main - adds positive no.s
- * @argc: integer value
- * @argv: list
- * Return: 0
- */
-
-int main(int argc, char const *argv[])
-{
-	int i = 0, coinUsed = 0, coin = 0;
-	int coins[] = {25, 10, 5, 2, 1}
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	if (isInteger(argv[1]))
-	{
-		i = atoi(argv[1]);
-		while (i > 0 && coin <= 4)
-		{
-			if (i >= coins[coin])
-			{
-				i -= coins[coin];
-				coinUsed++;
-			}
-			else
-			{
-				coin++;
-			}
-		}
-	}
-	printf("%i\n", coinUsed);
-	return (0);
+	 while (cents > 0)
+	 {
+		 if (cents >= 25)
+		 {
+			 cents -= 25;
+		 }
+		 if (cents >= 10)
+		 {
+			 cents -= 10;
+		 }
+		 if (cents >= 5)
+		 {
+			 cents -= 5;
+		 }
+		 else if (cents >= 2)
+		 {
+			 cents -= 2;
+		 }
+		 else if (cents >= 1)
+		 {
+			 cents -= 1;
+		 }
+		 ncoins += 1;
+	 }
+	 printf("%d\n". ncoins);
+	 return (0);
 }
